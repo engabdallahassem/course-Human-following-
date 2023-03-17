a44ac67f2de5ab2ca9cce7f0823eb606558b000e
Mahmoudgomaa001 <117765723+Mahmoudgomaa001@users.noreply.github.com>
2023-03-17 22:07:54 +0200

    Update import file.py

--- human_following/import file.py
+++ human_following/import file.py
@@ -6,18 +6,17 @@ import time
 from threading import Thread
 
 import sys
-sys.path.insert(0, '/var/www/html/earthrover')
-import util as ut
-ut.init_gpio()
+# sys.path.insert(0, '/var/www/html/earthrover')
+# import util as ut
+# ut.init_gpio()
 
-bject_to_track='person'
+# bject_to_track='person'
 
 #---------Flask----------------------------------------
 from flask import Flask, Response
 from flask import render_template
-app = Flask(__name__)
 
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)  # choose BCM numbering scheme
+
+
 
 #---------import 2 ----------------------------------------
