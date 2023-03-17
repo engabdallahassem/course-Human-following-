1f6a8f49deb79fa8612b464f7c7b7ac0f1a1ed0c
Mahmoudgomaa001 <117765723+Mahmoudgomaa001@users.noreply.github.com>
2023-03-17 22:10:07 +0200

    Update import file.py

--- human_following/import file.py
+++ human_following/import file.py
@@ -4,17 +4,10 @@ import numpy as np
 from PIL import Image
 import time
 from threading import Thread
-
 import sys
-# sys.path.insert(0, '/var/www/html/earthrover')
-# import util as ut
-# ut.init_gpio()
 
-# bject_to_track='person'
-
-#---------Flask----------------------------------------
-from flask import Flask, Response
-from flask import render_template
+# from flask import Flask, Response
+# from flask import render_template
 
 
 
