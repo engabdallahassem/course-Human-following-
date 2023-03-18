3c3a1a1e7c6cc2b66bb1d1a6a3161d96abe649a4
Mahmoudgomaa001 <117765723+Mahmoudgomaa001@users.noreply.github.com>
2023-03-18 02:12:40 +0200

    h

--- human_following/import_file.py
+++ human_following/import_file.py
@@ -1,10 +1,10 @@
 import common as cm
-import cv2
-import numpy as np
-from PIL import Image
-import time
-from threading import Thread
-import sys
+# import cv2
+# import numpy as np
+# from PIL import Image
+# import time
+# from threading import Thread
+# import sys
 
 # from flask import Flask, Response
 # from flask import render_template
